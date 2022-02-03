a88eadac2e4b68200e1e7095fd00700697e01d53
Shahinkhalajestani <vatharishok@gmail.com>
2022-02-03 15:33:25 +0330

    First commit

---
+++ src/main/java/com/shahintraining/mongowithspring/MongoWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.shahintraining.mongowithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoWithSpringApplication.class, args);
+	}
+
+}
