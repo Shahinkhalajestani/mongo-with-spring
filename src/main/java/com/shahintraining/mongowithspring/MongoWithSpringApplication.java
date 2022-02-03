90d70b462d614540bae0b206581a1a0114c1218c
Shahinkhalajestani <vatharishok@gmail.com>
2022-02-03 18:17:32 +0330

    used queries to find student.

--- src/main/java/com/shahintraining/mongowithspring/MongoWithSpringApplication.java
+++ src/main/java/com/shahintraining/mongowithspring/MongoWithSpringApplication.java
@@ -8,6 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -21,15 +24,25 @@ public class MongoWithSpringApplication {
     }
 
     @Bean
-    CommandLineRunner runner(StudentRepository repository) {
+    CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate) {
         return args -> {
-            Student shahin = Student.builder().firstName("shahin").lastName("khalajestani").email("shahin@GG.com")
+            String email = "shahin@GG.com";
+            Student shahin = Student.builder().firstName("shahin").lastName("khalajestani").email(email)
                     .address(Address.builder().country("iran").postCode("sdflkj@##@")
                             .state("Al").build()).gender(Gender.MALE)
                     .favouriteSubjects(List.of("computer Science", "Mathematics"))
                     .totalSpentInBooks(BigDecimal.TEN)
                     .created(LocalDateTime.now()).build();
-            repository.save(shahin);
+            Query query = new Query();
+            query.addCriteria(Criteria.where("email").is(email));
+            List<Student> students = mongoTemplate.find(query, Student.class);
+            if (students.size() > 1) {
+                throw new IllegalStateException("found many students with email" + email);
+            }
+            if (students.isEmpty()) {
+                System.out.println("inserting student"+ shahin);
+                repository.save(shahin);
+            }
         };
     }
 
